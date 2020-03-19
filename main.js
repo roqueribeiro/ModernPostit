4fe46cd1504e1e12a27f7e346a55ede76f64f6ff
Roque Ribeiro da Silva <roque.ribeiro@hotmail.com.br>
2020-03-19 14:20:22 -0300

    Add New Project

---
+++ main.js
@@ -0,0 +1,64 @@
+const {
+    app,
+    Menu,
+    Tray,
+    BrowserWindow
+} = require('electron')
+const path = require("path")
+const iconPath = path.join(__dirname, "favicon.ico");
+
+function createWindow() {
+
+    const win = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        transparent: true,
+        frame: false,
+        movable: false,
+        resizable: false,
+        maximizable: false,
+        minimizable: false,
+        alwaysOnTop: true,
+        kiosk: true,
+        skipTaskbar: true,
+        icon: iconPath
+    })
+
+    win.loadFile('index.html')
+    win.maximize()
+
+    appIcon = new Tray(iconPath);
+    const contextMenu = Menu.buildFromTemplate([{
+        label: "Show",
+        click: () => win.show()
+    }, {
+        label: "Hide",
+        click: () => win.hide()
+    }, {
+        label: "Close",
+        click: () => app.quit()
+    }]);
+
+    appIcon.on("double-click", () => {
+        if (mainWindow.isVisible()) mainWindow.hide();
+        else mainWindow.show();
+    });
+
+    appIcon.setToolTip("Modern Post-it");
+    appIcon.setContextMenu(contextMenu);
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
